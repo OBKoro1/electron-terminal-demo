a76640b4138b3a46a73664119261a455281b1e02
OBKoro1 <1677593011@qq.com>
2021-01-12 13:37:01 +0800

    feat: init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const processMessage = require('./processMessage')
 
 // 创建窗口
 function createWindow() {
+  // 创建窗口
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,10 +11,11 @@ function createWindow() {
       nodeIntegration: true, // 页面直接使用node的能力 用于引入node模块 执行命令
     },
   })
-  // 加载页面
+  debugger
+  // 加载本地页面
   win.loadFile('./src/index.html')
   win.webContents.openDevTools() // 打开控制台
-  // 通信逻辑   
+  // 主线程和渲染进程通信 
   const ProcessMessage = new processMessage(win)
   ProcessMessage.init()
 }
--- src/processMessage.js
+++ src/processMessage.js
@@ -6,33 +6,35 @@ class ProcessMessage {
    */
   constructor(win) {
     this.win = win
-    this.isActive = false
   }
   init() {
     this.watch()
+    this.on()
   }
-  // 初始化路径
-  initPath() {
-    this.updatePath(initPath)
-  }
-  // 监听执行的命令
+  // 监听渲染进程事件通信
   watch() {
     // 页面准备好了
     ipcMain.on('page-ready', () => {
       this.sendFocus()
     })
-    // 监听是否聚焦
+  }
+  // 监听窗口、app、等模块的事件
+  on() {
+    // 监听窗口是否聚焦
     this.win.on('focus', () => {
-      this.isActive = true
-      this.sendFocus()
+      this.sendFocus(true)
     })
     this.win.on('blur', () => {
-      this.isActive = false
-      this.sendFocus()
+      this.sendFocus(false)
     })
   }
-  sendFocus() {
-    this.win.webContents.send('win-focus', this.isActive)
+  /**
+   * 窗口聚焦事件发送
+   * @param {*} isActive 是否聚焦
+   */
+  sendFocus(isActive) {
+    // 主线程发送事件给窗口
+    this.win.webContents.send('win-focus', isActive)
   }
 }
 
